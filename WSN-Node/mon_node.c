df2e969d7abe13c6ddd2815be847393dcd598382
Xianglong.Wang <ge47vor@mytum.de>
2022-07-17 15:20:47 +0200

    finished network

--- WSN-Node/mon_node.c
+++ WSN-Node/mon_node.c
@@ -1,8 +1,10 @@
+// contiki headers
 #include "contiki.h"
 #include "net/rime/rime.h"	// Establish connections.
 #include "net/netstack.h"
 #include "lib/random.h"
 #include "dev/leds.h"
+#include "dev/button-sensor.h"
 
 // Standard C includes:
 #include <stdio.h>
@@ -13,6 +15,7 @@
 #include "cluster_head.h"
 
 // Define parameters of the network
+#define BROADCAST_EMERGENCY_CHANNEL 135
 #define BROADCAST_RIME_CHANNEL 129
 #define UNICAST_RIME_CHANNEL 146
 #define UNICAST_GATEWAY_CHANNEL 149
@@ -20,17 +23,19 @@
 #define DELAY_INTERVAL 0.5
 #define GATEWAY_INTERVAL 3.5
 #define CHANNEL 14
-#define POWER 3
+#define POWER_TO_GATEWAY 3
+#define POWER_REGULAR 1
+#define POWER_INITIAL 0
 #define MAX_N 20
 #define MAX_PAYLOAD 80
 
 // global variable
-static uint8_t node_id;		        // Stores node id
-static uint8_t head_id = 255;             // Stores head id
-static uint8_t gateway_id = 7;
+static uint8_t node_id;		           // Stores node id // @suppress("Type cannot be resolved")
+static uint8_t head_id = 255;          // Stores head id // @suppress("Type cannot be resolved")
+static uint8_t gateway_id = 7;         // Stores gateway id // @suppress("Type cannot be resolved")
 
-int to_report = 0;
-int report_end_sync = 0;
+int to_report = 0;                     // used by cm as flag to tell if it is reporting round
+int report_end_sync = 0;               // used by ch as flag to tell if send to gateway at this polling
 
 int is_cluster_head(){
 	return node_id == head_id;
@@ -70,6 +75,7 @@ PROCESS(node_initial, "Node self-organising the network");
 PROCESS(start_node_initial, "Used to start ");
 PROCESS(failure_countdown, "used by cm to count a ch failure");
 PROCESS(report_to_gateway, "used by ch to report to gateway.");
+PROCESS(really_emergency, "press the button and wait for help");
 
 AUTOSTART_PROCESSES(&sync_broadcasting,
 		            &uni_reporting,
@@ -77,7 +83,8 @@ AUTOSTART_PROCESSES(&sync_broadcasting,
 					&node_initial,
 					&start_node_initial,
 					&failure_countdown,
-					&report_to_gateway);
+					&report_to_gateway,
+					&really_emergency);
 
 
 // Defines the behavior of a cluster head received reporting frame.
@@ -89,8 +96,7 @@ static void reporting_recv(struct unicast_conn *c, const linkaddr_t *from){
 	/* Get packet's RSSI */
 	int16_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
 	/* Print address of sending node */
-	printf("Got RX packet (unicast) from: 0x%x%x, len: %d, RSSI: %d\r\n",
-			from->u8[0], from->u8[1],len,rssi);
+	printf("Got RX packet (unicast) from: 0x%x%x, len: %d, RSSI: %d\r\n", from->u8[0], from->u8[1],len,rssi);
 
 	static data_report rx_uni_report;
 	packetbuf_copyto(&rx_uni_report);
@@ -119,8 +125,7 @@ static void syncing_recv(struct broadcast_conn *c, const linkaddr_t *from)
 	/* Get packet's RSSI */
 	int16_t rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
 	/* Print address of sending node */
-	printf("Got RX packet (broadcast) from: 0x%x%x, len: %d, RSSI: %d\r\n",
-			from->u8[0], from->u8[1],len,rssi);
+	printf("Got RX packet (broadcast) from: 0x%x%x, len: %d, RSSI: %d\r\n", from->u8[0], from->u8[1],len,rssi);
     leds_off(LEDS_GREEN);
 
     if(Node_Is_Free()){
@@ -176,8 +181,8 @@ PROCESS_THREAD(uni_reporting, ev, data) {
 
 
 	PROCESS_BEGIN();
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, 14);
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, 3);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_REGULAR);
 
 	unicast_open(&unicast, UNICAST_RIME_CHANNEL, &unicast_call);
 
@@ -225,39 +230,6 @@ PROCESS_THREAD(uni_reporting, ev, data) {
 	PROCESS_END();
 }
 
-// used by cm to count down the time of last received sync,
-// if it is too long, consider the ch is failed, return itself to free node and run initial process.
-PROCESS_THREAD(failure_countdown, ev, data){
-	static uint8_t ch_failue_count = BROADCAST_INTERVAL;
-
-	PROCESS_EXITHANDLER( printf("[PROCESS_failure_countdown] Start the node! \n\r");)
-	PROCESS_BEGIN();
-	static struct etimer failure_count;
-
-	while(1) {
-		PROCESS_WAIT_EVENT();
-		if(ev == PROCESS_EVENT_MSG){
-			etimer_set(&failure_count, 2 * ch_failue_count * CLOCK_SECOND);
-		}
-		else if (ev == PROCESS_EVENT_TIMER){
-			if (etimer_expired(&failure_count) && (Node_Is_Free() == 0)){
-				if (Node_Is_Free() || is_cluster_head()){
-					printf("[PROCESS_failure_countdown] Do nothing.\n\r");
-				}
-				else{
-					Set_Free(1);
-				    head_id = 255;
-				    printf("[PROCESS_failure_countdown] Failure Detected.\n\r");
-				    process_post(&node_initial, PROCESS_EVENT_MSG, 0);
-				}
-			}
-		}
-	}
-
-	PROCESS_END();
-}
-
-
 // used by ch to broadcast sync frame
 PROCESS_THREAD(sync_broadcasting, ev, data) {
 
@@ -268,8 +240,8 @@ PROCESS_THREAD(sync_broadcasting, ev, data) {
 	PROCESS_EXITHANDLER(broadcast_close(&broadcast));
 	PROCESS_BEGIN();
 
-    NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, 14);
-    NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, 3);
+    NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+    NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_REGULAR);
 
     broadcast_open(&broadcast, BROADCAST_RIME_CHANNEL, &syncing_call);
 
@@ -278,7 +250,7 @@ PROCESS_THREAD(sync_broadcasting, ev, data) {
     	if (node_id == head_id && (ev == PROCESS_EVENT_MSG || ev == PROCESS_EVENT_TIMER)){
     		if (Remain_Duty()) {
     		    leds_on(LEDS_BLUE);
-    		    //fill the packet buffer
+
     		    if (Need_Report()){
     		        strcpy(tx_contacts.type, "Sync_Report");
     		        report_end_sync = 1;
@@ -291,11 +263,8 @@ PROCESS_THREAD(sync_broadcasting, ev, data) {
     		    process_post(&report_to_gateway, PROCESS_EVENT_MSG, 0);
 
     		    packetbuf_copyfrom(&tx_contacts ,15);
-
     		    broadcast_send(&broadcast);
-
-    		    printf("[PROCESS_sync_broadcasting] Sync message sent in channel %d with power: %d\r\n", 14, 3);
-    		    printf("%s \n\r", tx_contacts.type);
+    		    printf("[PROCESS_sync_broadcasting] Sync: %s sent in channel %d with power: %d\r\n", tx_contacts.type, 14, POWER_REGULAR);
 
     		    // self-event collect.
     		    int status = Random_Input();
@@ -319,15 +288,15 @@ PROCESS_THREAD(sync_broadcasting, ev, data) {
 
 // process that report the data to gateway
 PROCESS_THREAD(report_to_gateway, ev, data){
-	static uint8_t report_delay_interval = GATEWAY_INTERVAL;
+	static uint8_t report_delay_interval = GATEWAY_INTERVAL; // @suppress("Type cannot be resolved")
 
 	PROCESS_EXITHANDLER(unicast_close(&unicast);)
 
     static struct etimer delay;
 
 	PROCESS_BEGIN();
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, 14);
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, 3);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_TO_GATEWAY);
 
 	unicast_open(&unicast, UNICAST_GATEWAY_CHANNEL, &unicast_call);
 
@@ -379,8 +348,8 @@ PROCESS_THREAD(head_change, ev, data){
 	PROCESS_EXITHANDLER(broadcast_close(&broadcast));
 	PROCESS_BEGIN();
 
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, 14);
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, 3);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_REGULAR);
 
 	broadcast_open(&broadcast, BROADCAST_RIME_CHANNEL, &syncing_call);
 
@@ -404,7 +373,6 @@ PROCESS_THREAD(head_change, ev, data){
 				    packetbuf_copyfrom(&tx_contacts ,100);
 				    broadcast_send(&broadcast);
 
-				    printf("Switch message sent in channel %d with power: %d\r\n", 14, 3);
 				    process_post(&failure_countdown, PROCESS_EVENT_MSG, 0);
 				    Reset_Topo();
 				}
@@ -421,17 +389,48 @@ PROCESS_THREAD(head_change, ev, data){
 	PROCESS_END();
 }
 
+// used by cm to count down the time of last received sync,
+// if it is too long, consider the ch is failed, return itself to free node and run initial process.
+PROCESS_THREAD(failure_countdown, ev, data){
+	static uint8_t ch_failue_count = BROADCAST_INTERVAL;
+
+	PROCESS_EXITHANDLER( printf("[PROCESS_failure_countdown] Start the node! \n\r");)
+	PROCESS_BEGIN();
+	static struct etimer failure_count;
+
+	while(1) {
+		PROCESS_WAIT_EVENT();
+		if(ev == PROCESS_EVENT_MSG){
+			etimer_set(&failure_count, 2 * ch_failue_count * CLOCK_SECOND);
+		}
+		else if (ev == PROCESS_EVENT_TIMER){
+			if (etimer_expired(&failure_count) && (Node_Is_Free() == 0)){
+				if (Node_Is_Free() || is_cluster_head()){
+					printf("[PROCESS_failure_countdown] Do nothing.\n\r");
+				}
+				else{
+					Set_Free(1);
+				    head_id = 255;
+				    printf("[PROCESS_failure_countdown] Failure Detected.\n\r");
+				    process_post(&node_initial, PROCESS_EVENT_MSG, 0);
+				}
+			}
+		}
+	}
+
+	PROCESS_END();
+}
+
 // process used for free node to self-organized network
 PROCESS_THREAD(node_initial, ev, data){
 
 	PROCESS_EXITHANDLER( printf("[PROCESS_node_initial] Join or construct the network. \n\r");)
 	PROCESS_BEGIN();
 
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, 14);
-	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, 3);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_INITIAL);
 
 	static uint8_t overhead = 4;
-
 	static struct etimer delay;
 
 	while(1){
@@ -469,3 +468,58 @@ PROCESS_THREAD(start_node_initial, ev, data){
 
 	PROCESS_END();
 }
+
+// When button pressed, send directly to gateway for help
+PROCESS_THREAD(really_emergency, ev, data){
+
+	PROCESS_EXITHANDLER(broadcast_close(&broadcast));
+	PROCESS_BEGIN();
+	button_sensor.configure(BUTTON_SENSOR_CONFIG_TYPE_INTERVAL, CLOCK_SECOND*2);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL, CHANNEL);
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_TXPOWER, POWER_TO_GATEWAY);
+
+	broadcast_open(&broadcast, BROADCAST_EMERGENCY_CHANNEL, &syncing_call);
+
+	static syncing tx_emergency;
+	strcpy(tx_emergency.type, "Emergency");
+	tx_emergency.nodeid = node_id;
+
+	while(1) {
+	    PROCESS_WAIT_EVENT();
+	    if (ev == sensors_event) {
+	        if (data == &button_sensor) {
+	        	if (button_sensor.value(BUTTON_SENSOR_VALUE_TYPE_LEVEL) == BUTTON_SENSOR_PRESSED_LEVEL) {
+	                leds_on(LEDS_RED);
+	            }
+	            else if(button_sensor.value(BUTTON_SENSOR_VALUE_TYPE_LEVEL) == BUTTON_SENSOR_RELEASED_LEVEL) {
+	        	    leds_off(LEDS_ALL);
+	                }
+	            }
+	        }
+	    else if(ev == button_press_duration_exceeded){
+	        leds_on(LEDS_RED);
+
+	        packetbuf_copyfrom(&tx_emergency ,20);
+	        broadcast_send(&broadcast);
+	        printf("[PROCESS_really_emergency] Emergency Node: %d, with type: %s.\n\r", tx_emergency.nodeid, tx_emergency.type);
+	    }
+	}
+
+	PROCESS_END();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
